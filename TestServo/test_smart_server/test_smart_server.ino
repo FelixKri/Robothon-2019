e20b206603deeddb2cd758ca5e76b8c273391ca7
Siegfried Schoendorfer <44769692+Siegfried-Schoendorfer@users.noreply.github.com>
2019-04-13 05:02:07 +0200

    Create test_smart_server.ino

---
+++ TestServo/test_smart_server/test_smart_server.ino
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeAuriga.h>
+#include "Wire.h"
+
+MeSmartServo mysmartservo(PORT5);
+
+void setup(){
+   
+    mysmartservo.begin(115200);
+    mysmartservo.assignDevIdRequest();
+    delay(50);
+
+        mysmartservo.moveTo(1,90,30);
+        delay(1000);
+        mysmartservo.setInitAngle(1);
+         delay(1000);
+        mysmartservo.moveTo(1,-90,30);
+         delay(1000);
+        mysmartservo.setInitAngle(1);
+         delay(1000);    
+}
+
